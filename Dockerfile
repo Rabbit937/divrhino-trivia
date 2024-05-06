623ad6d22d017f70b20b60d52211518d1b462eef
Rabbit937 <hackrabbit@qq.com>
2024-05-06 17:30:50 +0800

    feat: 使用air hot reload，docker环境无法使用

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,9 @@ FROM golang:latest
 
 WORKDIR /usr/src/app
 
+RUN go install github.com/cosmtrek/air@latest
+
+ENTRYPOINT ["air"]
+
 COPY . .
 RUN go mod tidy
--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Hackrabbi")
+		return c.SendString("Hello, Hackrabb")
 	})
 
 	app.Listen(":3000")
